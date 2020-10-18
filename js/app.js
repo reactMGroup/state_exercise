a92633cc938f57dce7e6465b8734e61e486cbdb7
Kirill <m-group-react@tutanota.com>
2020-10-18 17:43:29 +0300

    Parent component shows picked colors.

--- js/app.js
+++ js/app.js
@@ -34,15 +34,72 @@ class ToggleBox extends React.Component {
 
 class ColorPanel extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.itemUpdated = this.itemUpdated.bind(this);
+        this.state = {
+            items: [{
+                color: "Green",
+                disabled: false
+            },
+            {
+                color: "Red",
+                disabled: true
+            },
+            {
+                color: "Yellow",
+                disabled: false
+            },
+            {
+                color: "Gray",
+                disabled: false
+            },
+            {
+                color: "Lightgray",
+                disabled: false
+            }]
+        }
+    }
+
+    itemUpdated(id, newDisabled) {
+        this.setState(prev => {
+            const newState = prev.items.map(item => {
+                if (id === item.color) {
+                    return {
+                        color: item.color,
+                        disabled: newDisabled
+                    };
+                } else {
+                    return item;
+                }
+
+            });
+            return { items: newState };
+        });
+    }
+
     render() {
         return (
             <article>
                 <h2>Which colors do you want?</h2>
                 <h5>(disable with a click)</h5>
                 <nav id='colorsNav'>
-                    <ColorInput color='Green' />
-                    <ColorInput color='Red' disabled={true} />
+                    {
+                        this.state.items.map(
+                            item => {
+                                return (<ColorInput key={item.color} color={item.color} disabled={item.disabled} processClick={this.itemUpdated} />);
+                            }
+                        )
+                    }
                 </nav>
+                <label>You picked the follwoing colors:</label>
+                <ul>
+                    {
+                        this.state.items
+                            .filter(item => !item.disabled)
+                            .map(picked => (<li key={picked.color}>{picked.color}</li>))
+                    }
+                </ul>
             </article>
         )
     }
